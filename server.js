d5838648f8c11e21e613d4e52110537263fdf62b
yifenyifeng <archevanliu@gmail.com>
2024-11-17 12:50:15 -0500

    Fix dynamic port binding

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-// const port = 3000;
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
 app.use(cors());
 
 // Connect to MongoDB
