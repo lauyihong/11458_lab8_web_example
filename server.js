b382a0c76933c0fafb4fbc0980ca2127720d9a9c
yifenyifeng <archevanliu@gmail.com>
2024-11-17 12:53:03 -0500

    Fix dynamic port binding

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const cors = require('cors');
 const app = express();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+
 
 app.use(cors());
 
