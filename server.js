d4df95f7b6eb970a4794bf221050c5dc6526383b
yifenyifeng <archevanliu@gmail.com>
2024-11-17 12:48:02 -0500

    Fix port binding for Heroku

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
-
+// const port = 3000;
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 app.use(cors());
 
 // Connect to MongoDB
